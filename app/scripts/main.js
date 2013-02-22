a331f262932805e21118af907e1cfb001bf78c0e
Frédéric Camblor <fcamblor@gmail.com>
2013-02-22 02:06:48 +0100

    declared require-handlebars-plugin + underscore dependencies in requiresjs and component.json

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,27 @@
 require.config({
     paths: {
         jquery: '../components/jquery/jquery',
+        underscore: '../components/underscore/underscore',
+        json2: '../components/require-handlebars-plugin/hbs/json2',
+        handlebars: '../components/require-handlebars-plugin/Handlebars',
+        i18nprecompile: '../components/require-handlebars-plugin/hbs/i18nprecompile',
+        hbs: '../components/require-handlebars-plugin/hbs',
         bootstrap: 'vendor/bootstrap'
     },
     shim: {
         bootstrap: {
             deps: ['jquery'],
             exports: 'jquery'
+        },
+        underscore: {
+            deps: [],
+            exports: '_'
         }
+    },
+    deps: [ 'hbs' ],
+    // hbs particular configuration properties
+    hbs: {
+        disableI18n: true // Support for i18n is useless for the moment...
     }
 });
 
